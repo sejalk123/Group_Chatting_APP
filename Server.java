c02d09495991c37c2eeefca5d55a72d3856dfb1b
Sejal Kalambhe <89303909+sejalk123@users.noreply.github.com>
2023-07-24 20:34:05 +0530

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,54 @@
+package group.chatting.application;
+ import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Server implements Runnable {
+    Socket socket;
+    
+    public static Vector client = new Vector();   //we can use map as well instead of vector
+    public Server(Socket socket){
+        try{
+            this.socket=socket;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void run(){
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            client.add(writer);
+            
+            while(true){
+                String data = reader.readLine().trim();
+                System.out.println("Received "+data);
+                
+                for(int i=0;i<client.size();i++){
+                    try{
+                        BufferedWriter bw = (BufferedWriter)client.get(i);    
+                        bw.write(data);
+                        bw.write("\r\n");    //
+                        bw.flush();           //msg sent successfully
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) throws Exception{
+        ServerSocket s = new ServerSocket(2003);
+        while(true){
+            Socket socket = s.accept();    //for accepting the clients with the help of serverSocket
+            Server server = new Server(socket);
+            Thread thread = new Thread(server);
+            thread.start();         //for calling run method
+        }
+    }
+}
